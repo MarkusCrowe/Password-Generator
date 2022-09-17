74693b473af556083449db8fba956fe160bf209b
Romain Auger <romainauger@wanadoo.fr>
2022-09-17 13:13:12 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "0123456789";
+const dataSymbols = "$¨*ù%:;,?/!&'()§-_°\"";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
+
+function generatePassword() {
+    // console.log("Je génère un MDP");
+    let data = [];
+    let password = "";
+
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (number.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+
+    if (data.length === 0) {
+        alert("Veuillez selectionner des critères");
+        return;
+    };
+
+    for ( i = 0 ; i < rangeValue.value; i++) {
+        password += data[Math.floor(Math.random() * data.length)];
+        console.log(password);
+    }
+
+    passwordOutput.value = password;
+    passwordOutput.select();
+    document.execCommand("copy");
+
+    generateButton.textContent = "Copié!";
+
+    setTimeout(() => {
+        generateButton.textContent = "Générer un autre Mot de passe";
+    }, 2000);
+};
+
+generateButton.addEventListener("click", generatePassword);
